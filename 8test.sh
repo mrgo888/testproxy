825bb9c5dc1647a50db9b812bf532bf938e18fbe
mrgo88 <86548440+mrgo88@users.noreply.github.com>
2021-06-28 00:10:12 +0700

    Update 8test.sh

--- 8test.sh
+++ 8test.sh
@@ -34,9 +34,9 @@ timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
 flush
-auth none
+auth iponly
 
-$(awk -F "/" '{print "auth none\n" \
+$(awk -F "/" '{print "auth iponly\n" \
 "allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
