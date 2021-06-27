d68af4226f5898869fb7866ec8117331e6959a64
mrgo88 <86548440+mrgo88@users.noreply.github.com>
2021-06-27 23:28:17 +0700

    Update 8test.sh

--- 8test.sh
+++ 8test.sh
@@ -54,8 +54,12 @@ setgid 65535
 setuid 65535
 stacksize 6291456 
 flush
-auth none
-$(awk -F "/" '{print "proxy -6 -n -a -p" $4 " -e" $3}' ${WORKDATA})
+auth strong
+users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
+$(awk -F "/" '{print "auth strong\n" \
+"allow " $1 "\n" \
+"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
+"flush\n"}' ${WORKDATA})
 EOF
 }
 
