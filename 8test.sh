8735ee80f1ea2ea017ccbfda7f43564ceaabfc2d
mrgo88 <86548440+mrgo88@users.noreply.github.com>
2021-06-28 14:12:00 +0700

    Update 8test.sh

--- 8test.sh
+++ 8test.sh
@@ -35,9 +35,8 @@ setgid 65535
 setuid 65535
 flush
 auth iponly
-
 $(awk -F "/" '{print "auth none\n" \
-"allow" $1 "\n" \
+"allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
@@ -45,7 +44,7 @@ EOF
 
 gen_proxy_file_for_user() {
     cat >proxy.txt <<EOF
-$(awk -F "/" '{print $3 ":" $4 ":" $1 }' ${WORKDATA})
+$(awk -F "/" '{print $3 ":" $4 }' ${WORKDATA})
 EOF
 }
 
