70fc1ef809a9a6c76a44759a9627aa69dc11340a
mrgo88 <86548440+mrgo88@users.noreply.github.com>
2021-06-28 10:50:43 +0700

    Update 8test.sh

--- 8test.sh
+++ 8test.sh
@@ -35,9 +35,9 @@ setgid 65535
 setuid 65535
 flush
 auth iponly
-allow * 113.173.156.215/0
+
 $(awk -F "/" '{print "auth none\n" \
-"allow * 113.173.156.215/0 \n" \
+"allow * 113.173.156.215 \n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
